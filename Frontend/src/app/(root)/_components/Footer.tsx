cc4d4e858ad209b9203f0d005e6d10c265e01f96
Miguel Mascareno <mmascare@ucsc.edu>
2024-10-11 00:07:23 -0700

    reformatted landing page

---
+++ Frontend/src/app/(root)/_components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return ( 
+    <footer className="bg-gray-800 text-white py-6">
+    <div className="container mx-auto text-center">
+      <p>&copy; 2024 RepIt. All rights reserved.</p>
+    </div>
+    </footer>
+   );
+}
+ 
+export default Footer;
