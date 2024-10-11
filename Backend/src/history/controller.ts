a1886855d93b531e98519013aeb972254837423b
DDublue <theonly1living@gmail.com>
2024-10-11 15:32:30 -0700

    Created GET endpoints (untested); still need to determine date format

--- Backend/src/history/controller.ts
+++ Backend/src/history/controller.ts
@@ -1,32 +1,53 @@
-// // import stuff
+import { Controller, Get, Path, Route } from "tsoa";
+import { HistoryService } from "./service";
+import { History } from ".";
 
+// Bug: path decorator 
+@Route('history')
+export class HistoryController extends Controller {
 
+  // Get entire user history
+  @Get("{userId}")
+  public async getHistoryByAll(
+    @Path() userId: string
+  ): Promise<History> {
+    return new HistoryService().getHistoryByAll(userId);
+  };
 
+  // Get user history by year
+  @Get("{userId}/{year}")
+  public async getHistoryByYear(
+    @Path() userId: string,
+    @Path() year: string
+  ): Promise<History> {
+    return new HistoryService().getHistoryByYear(userId, year);
+  }
+  
+  // Get user history by month
+  @Get("{userId}/{month}")
+  public async getHistoryByMonth(
+    @Path() userId: string,
+    @Path() month: string
+  ): Promise<History> {
+    return new HistoryService().getHistoryByMonth(userId, month);
+  }
+  
+  // Get user history by week
+  @Get("{userId}/{week}")
+  public async getHistoryByWeek(
+    @Path() userId: string,
+    @Path() month: string
+  ): Promise<History> {
+    return new HistoryService().getHistoryByWeek(userId, month);
+  }
+  
+  // Get user history by day
+  @Get("{userId}/{day}")
+  public async getHistoryByDay(
+    @Path() userId: string,
+    @Path() day: string
+  ): Promise<History> {
+    return new HistoryService().getHistoryByDay(userId, day);
+  }
 
-// @Route("history")
-// export class UserController extends Controller {
-//   private historyService: HistoryService = new HistoryService();
-
-//   // get entire user history
-//   @Get("{userId}")
-//   public async getUserHistory(@Path() userId: number): Promise<History | null> {
-//     return this.historyService.getUserHistory(userId);
-//   }
-
-//   // get user history by month/year
-//   // get user history by week
-//   // get user history by day
-//   // get user history by year
-
-
-
-//   @Get()
-//   public async getAllUsers(): Promise<User[]> {
-//     return this.userService.getAllUsers();
-//   }
-
-//   @Post()
-//   public async createUser(@Body() requestBody: { name: string; email: string }): Promise<User> {
-//     return this.userService.createUser(requestBody.name, requestBody.email);
-//   }
-// }
+}
