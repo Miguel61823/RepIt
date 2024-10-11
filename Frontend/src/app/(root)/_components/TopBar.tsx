075919a124e876bc6f448567d0ad7f16420e9187
Miguel Mascareno <mmascare@ucsc.edu>
2024-10-11 10:55:55 -0700

    big push

--- Frontend/src/app/(root)/_components/TopBar.tsx
+++ Frontend/src/app/(root)/_components/TopBar.tsx
@@ -1,16 +1,19 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 const Topbar = () => {
   return ( 
-    <header className="bg-blue-600 text-white p-4">
-    <div className="container mx-auto flex justify-between items-center">
+    <header className="bg-blue-600 text-black dark:text-white p-4 ">
+    <div className="container mx-auto flex items-center ">
       <h1 className="text-2xl font-bold">RepIt App</h1>
-      <nav>
-        <ul className="flex space-x-4">
+      <nav className="">
+        <ul className="flex space-x-4 justify-right">
           <li><a className="hover:underline">Features</a></li>
           <li><a className="hover:underline">Pricing</a></li>
           <li><a className="hover:underline">Contact</a></li>
           <li><a className="hover:underline">Login</a></li>
         </ul>
       </nav>
+      <ModeToggle/>
     </div>
   </header>
    );
