5d8ba51bf942abd9051b8480a1d599614899948c
Miguel Mascareno <mmascare@ucsc.edu>
2024-10-11 13:30:00 -0700

    frontend stuff

--- Frontend/src/app/(root)/_components/plans.tsx
+++ Frontend/src/app/(root)/_components/plans.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const Plans = () => {
   return ( 
     <section id="pricing" className="mb-12">
     <h3 className="text-2xl font-bold mb-6">Pricing Plans</h3>
     <div className="grid md:grid-cols-3 gap-8">
-      <div className="bg-blue-300 p-6 rounded-lg shadow-md  text-black">
+      <div className="bg-blue-200 p-6 rounded-lg shadow-md  text-blue-950">
         <h4 className="text-xl font-semibold mb-2">Basic</h4>
         <p className="text-3xl font-bold mb-4">$9.99<span className="text-sm font-normal">/month</span></p>
         <ul className="mb-6">
@@ -11,11 +13,11 @@ const Plans = () => {
           <li>✓ Basic nutrition tracking</li>
           <li>✓ Limited progress analytics</li>
         </ul>
-        <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300">
+        <Button variant="wide">
           Choose Plan
-        </button>
+        </Button>
       </div>
-      <div className="bg-blue-300 p-6 rounded-lg shadow-md border-2  text-black">
+      <div className="bg-blue-200 p-6 rounded-lg shadow-md  text-blue-950">
         <h4 className="text-xl font-semibold mb-2">Pro</h4>
         <p className="text-3xl font-bold mb-4">$19.99<span className="text-sm font-normal">/month</span></p>
         <ul className="mb-6">
@@ -24,11 +26,11 @@ const Plans = () => {
           <li>✓ Full progress analytics</li>
           <li>✓ Coach support</li>
         </ul>
-        <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300">
+        <Button variant="wide">
           Choose Plan
-        </button>
+        </Button>
       </div>
-      <div className="bg-blue-300 p-6 rounded-lg shadow-md text-black">
+      <div className="bg-blue-200 p-6 rounded-lg shadow-md text-blue-950">
         <h4 className="text-xl font-semibold mb-2">Elite</h4>
         <p className="text-3xl font-bold mb-4">$29.99<span className="text-sm font-normal">/month</span></p>
         <ul className="mb-6">
@@ -37,9 +39,9 @@ const Plans = () => {
           <li>✓ 1-on-1 coaching sessions</li>
           <li>✓ Premium content access</li>
         </ul>
-        <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300">
+        <Button variant="wide">
           Choose Plan
-        </button>
+        </Button>
       </div>
     </div>
   </section>
--- Frontend/src/app/(root)/page.tsx
+++ Frontend/src/app/(root)/page.tsx
@@ -6,31 +6,32 @@ import Features from './_components/Features';
 import Footer from './_components/Footer';
 import { SignInButton } from '@clerk/nextjs';
 import { SignedOut } from '@clerk/clerk-react';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div className="dark:bg-blue-900  bg-blue-200 min-h-screen  dark:text-blue-200 text-blue-600">
+    <div className="dark:bg-blue-900  bg-blue-200 min-h-screen  dark:text-blue-200 text-blue-950">
 
       <Topbar/>
-      <main className="container mx-auto mt-8 px-4 h-screen">
+      <main className="container mx-auto mt-8 px-4">
         <SignedOut>
           <section className="text-center mb-12">
             <h2 className="text-4xl font-bold mb-4">Transform Your Fitness Journey</h2>
             <p className="text-xl mb-6">Achieve your health goals with personalized workouts plans</p>
             
               <SignInButton mode="modal">
-                <button className="bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-green-600 transition duration-300">
+                <Button className="bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-blue-600">
                   Get Started
-                </button>
+                </Button>
               </SignInButton>
             
           </section>
           
           <Features/>
           <Plans/>
-          <Footer/>
+          
         </SignedOut>
-
+      <Footer/>
       </main>
       
         {/* <Dashboard/> */}
