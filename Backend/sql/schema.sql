425fc8c781d3409e1f2005e0c5ddb264ad0091f3
DDublue <theonly1living@gmail.com>
2024-10-12 13:55:53 -0700

    Added necessary db tables for workout history (skeleton/commented out)

--- Backend/sql/schema.sql
+++ Backend/sql/schema.sql
@@ -1,5 +1,33 @@
 DROP TABLE IF EXISTS user;
+
+-- DROP TABLE IF EXISTS workout_instance;
+-- DROP TABLE IF EXISTS exercise_instance;
+-- DROP TABLE IF EXISTS workout_excerises;
+-- DROP TABLE IF EXISTS set_instance;
+
 CREATE TABLE user(
   email TEXT,
   first_name TEXT
 );
+
+-- CREATE TABLE workout_instance(
+--   workout_id ,
+--   user_id ,
+--   date_completed ,
+-- );
+-- CREATE TABLE exercise_instance(
+--   exercise_id ,
+  
+-- );
+-- CREATE TABLE workout_excerises(
+--   wo_instance_id ,
+--   exercise_id ,
+-- );
+-- CREATE TABLE set_instance(
+--   exercise_id ,
+--   order ,
+--   weight ,
+--   reps ,
+--   machine ,
+-- );
+
