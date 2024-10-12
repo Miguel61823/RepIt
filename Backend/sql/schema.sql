5ecd39eae27c8708e7273be8008823e392ed82c1
DDublue <theonly1living@gmail.com>
2024-10-12 16:35:57 -0700

    Updated fields to history db tables (change if needed)

--- Backend/sql/schema.sql
+++ Backend/sql/schema.sql
@@ -1,37 +1,31 @@
 DROP TABLE IF EXISTS user;
 
--- DROP TABLE IF EXISTS workout_instance;
--- DROP TABLE IF EXISTS exercise_instance;
--- DROP TABLE IF EXISTS workout_excerises;
--- DROP TABLE IF EXISTS set_instance;
+DROP TABLE IF EXISTS workout_instance;
+DROP TABLE IF EXISTS exercise_instance;
+DROP TABLE IF EXISTS set_instance;
 
 CREATE TABLE user(
   email TEXT,
   first_name TEXT
 );
 
--- CREATE TABLE workout_instance(
---   id ,
---   workout_id ,
---   user_id ,
---   date_completed ,
--- );
--- CREATE TABLE exercise_instance(
---   id ,
---   exercise_id ,
---   
--- );
--- CREATE TABLE workout_excerises(
---   id ,
---   wo_instance_id ,
---   exercise_id ,
--- );
--- CREATE TABLE set_instance(
---   id ,
---   exercise_id ,
---   order ,
---   weight ,
---   reps ,
---   machine ,
--- );
+CREATE TABLE workout_instance(
+  id BIGSERIAL PRIMARY KEY,
+  user_id BIGINT,
+  date_completed TIMESTAMPTZ, 
+  workout_id REFERENCES workout(id),
+);
+CREATE TABLE exercise_instance(
+  id BIGSERIAL PRIMARY KEY,
+  workout_instance_id REFERENCES workout_instance(id),
+  exercise_id REFERENCES exercise(id),
+);
+CREATE TABLE set_instance(
+  id BIGSERIAL PRIMARY KEY,
+  order INT,
+  weight INT,
+  reps INT,
+  machine TEXT,
+  exercise_instance_id REFERENCES exercise_instance(id),
+);
 
