218858ed7e72812ee328284b5a22756e8e1d11ee
DDublue <theonly1living@gmail.com>
2024-10-11 20:28:51 -0700

    Updated history dir to deal solely w/instances (of workouts, exercises, and sets); still need db tables

--- Backend/src/history/index.ts
+++ Backend/src/history/index.ts
@@ -1,20 +1,49 @@
-export interface History {
+/**
+ * /
+ * An instance of a workout, completed by a specific user.
+ * 
+ * @interface WorkoutInstance
+ * @field {number} workoutId: Id of the workout (not the instance)
+ * @field {string} userId: Id of the user of this workout instance
+ * @field {string} dataCompleted: Completion date of this workout instance
+ * @field {ExerciseInstance[]} exercises: List of exercises of the original workout
+ */
+export interface WorkoutInstance {
+  workoutId: number;
   userId: string;
-  workouts: Workout[];
+  dateCompleted: string;
+  exercises: ExerciseInstance[];
 }
 
-export interface Workout {
-  userId: string;
-  date: string;
-  exercises: Exercise[];
-}
-
-export interface Exercise {
+/**
+ * 
+ * An exercise instance associated with its respective WorkoutInstance.
+ * 
+ * @interface ExerciseInstance
+ * @field {number} exerciseId: Id of the exercise (not the instance)
+ * @field {string} name: Name of the exercise
+ * @field {SetInstance[]} sets: List of sets done for this exercise instance
+ */
+export interface ExerciseInstance {
+  exerciseId: number;
   name: string;
-  sets: Set[];
+  sets: SetInstance[];
 }
 
-export interface Set {
+/**
+ * 
+ * An set instance associated with its respective ExerciseInstance.
+ * 
+ * @interface SetInstance
+ * @field {number} exerciseId: Id of the exercise
+ * @field {number} order: The current set iteration for its exercise instance
+ * @field {string} exercise: Name of the exercise
+ * @field {number} weight: The weight used for this set instance
+ * @field {number} reps: The reps completed for this set instance
+ * @field {string} machine: The machine used for this set instance
+ */
+export interface SetInstance {
+  exerciseId: number;
   order: number;
   exercise: string;
   weight: number;
