91aa861a57df1c79e93bed193594a99ccd480f20
Miguel61823 <mmascare@ucsc.edu>
2024-10-14 19:29:01 -0700

    bunch of stuff plus jest support and

--- WorkoutService/src/history/index.ts
+++ WorkoutService/src/history/index.ts
@@ -1,49 +1,50 @@
 /**
  * 
- * An instance of a workout, completed by a specific user.
+ * A workout, completed by a specific user.
  * 
- * @interface WorkoutInstance
+ * @interface Workout
  * @field {number} workoutId: Reference id of the workout
- * @field {string} userId: Id of the user of this workout instance
- * @field {string} dataCompleted: Completion date of this workout instance
- * @field {ExerciseInstance[]} exercises: List of exercises of the original workout
+ * @field {string} userId: Id of the user of this workout 
+ * @field {string} dataCompleted: Completion date of this workout 
+ * @field {Exercise[]} exercises: List of exercises of the original workout
  */
-export interface WorkoutInstance {
-  workoutId: number;
+export interface Workout {
+  id: number;
   name: string;
   userId: string;
   dateCompleted: string;
-  exercises: ExerciseInstance[];
+  exercises: Exercise[];
 }
 
 /**
  * 
- * An exercise instance associated with its respective WorkoutInstance.
+ * An exercise associated with its respective Workout.
  * 
- * @interface ExerciseInstance
+ * @interface Exercise
  * @field {number} exerciseId: Reference id of the exercise
  * @field {string} name: Name of the exercise
  * @field {number} number: current iteration of exercise in workout
- * @field {SetInstance[]} sets: List of sets done for this exercise instance
+ * @field {Set[]} sets: List of sets done for this exercise 
  */
-export interface ExerciseInstance {
-  exerciseId: number;
+export interface Exercise {
+  id: number;
   name: string;
   number: number;
-  sets: SetInstance[];
+  sets: Set[];
 }
 
 /**
  * 
- * An set instance associated with its respective ExerciseInstance.
+ * A set associated with its respective Exercise.
  * 
- * @interface SetInstance
- * @field {number} order: The current set iteration for its exercise instance
- * @field {number} weight: The weight used for this set instance
- * @field {number} reps: The reps completed for this set instance
- * @field {string} machine: The machine used for this set instance
+ * @interface Set
+ * @field {number} order: The current set iteration for its exercise 
+ * @field {number} weight: The weight used for this set 
+ * @field {number} reps: The reps completed for this set 
+ * @field {string} machine: The machine used for this set 
  */
-export interface SetInstance {
+export interface Set {
+  id: number;
   order: number;
   weight: number;
   reps: number;
