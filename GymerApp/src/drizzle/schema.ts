19aadf9b4c149c1858c33f5956aebfd76ce5cb03
DDublue <theonly1living@gmail.com>
2024-10-18 10:27:26 -0700

    Updated fields (need to add/change tables; not final)

--- GymerApp/src/drizzle/schema.ts
+++ GymerApp/src/drizzle/schema.ts
@@ -1,18 +1,21 @@
-import { index, jsonb, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { index, jsonb, pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-export const UserTable = pgTable("user", {
+export const MemberTable = pgTable("member", {
+  id: uuid("id").primaryKey().defaultRandom(),
   email: text("email").notNull(),
   first_name: text("first_name").notNull(),
   last_name: text("last_name").notNull()
 })
 
-export const WorkoutTable = pgTable("workout", {
-  id: uuid("id").primaryKey().defaultRandom(),
+export const WorkoutsTable = pgTable("workouts", {
+  id: serial("id").primaryKey(),
+  user_id: uuid("user_id")
+    .notNull()
+    .references(() => MemberTable.id, { onDelete: "cascade" }),
   title: text("title").notNull(),
   description: text("description"),
   date_completed: timestamp("date_completed"),
-  exercises: jsonb().notNull(),
-  clerk_user_id: text("clerk_user_id").notNull()
+  exercises: jsonb().notNull()
 }, table => ({
-  clerk_user_id_index: index("clerk_user_id_index").on(table.clerk_user_id)
+  user_id_index: index("user_id_index").on(table.user_id)
 }))
