904b59f90b150272486ef68348d3d2b5d60e71cd
DDublue <theonly1living@gmail.com>
2024-10-17 22:47:33 -0700

    Full footer

--- GymerApp/src/app/_components/Footer.tsx
+++ GymerApp/src/app/_components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return ( 
-    <footer className="bg-gray-800 text-white py-6">
+    <footer className="bg-gray-800 text-white py-6 bottom-0 w-full">
     <div className="container mx-auto text-center">
       <p>&copy; 2024 RepIt. All rights reserved.</p>
     </div>
