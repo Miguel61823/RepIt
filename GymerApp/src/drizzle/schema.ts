59286a0efdeb28324386a92ddb4faeada92c6cfd
DDublue <theonly1living@gmail.com>
2024-10-17 17:28:24 -0700

    Moved current db schemas into /drizzle w/added fields

---
+++ GymerApp/src/drizzle/schema.ts
@@ -0,0 +1,18 @@
+import { index, jsonb, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const user = pgTable("user", {
+  email: text("email").notNull(),
+  first_name: text("first_name").notNull(),
+  last_name: text("last_name")
+})
+
+export const WorkoutTable = pgTable("workout", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: text("title").notNull(),
+  description: text("description"),
+  date_completed: timestamp("date_completed"),
+  exercises: jsonb().notNull(),
+  clerk_user_id: text("clerk_user_id").notNull()
+}, table => ({
+  clerk_user_id_index: index("clerk_user_id_index").on(table.clerk_user_id)
+}))
