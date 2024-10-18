0c8b7f02e85b84566ec938202478869e98aaf5ed
Miguel Mascareno <mmascare@ucsc.edu>
2024-10-18 00:05:17 -0700

    added more routing options (hidden but intuitive) modified dashboard

--- GymerApp/src/app/dashboard/page.tsx
+++ GymerApp/src/app/dashboard/page.tsx
@@ -2,11 +2,11 @@
 import React from 'react';
 import Topbar from '../_components/TopBar';
 import Footer from '../_components/Footer';
-import { Button } from '@/components/ui/button';
-import Features from '../_components/Features';
-import Link from 'next/link';
+// import { Button } from '@/components/ui/button';
+// import Features from '../_components/Features';
+// import Link from 'next/link';
 
-import Board from './_components/fitness';
+// import Board from './_components/fitness';
 import HistoryList from './_components/history';
 
 
@@ -15,11 +15,12 @@ export default function Dashboard() {
   return ( 
     <div className="flex flex-col dark:bg-black  bg-white min-h-screen  dark:text-white text-black">
       <Topbar/>
-      <main className="container flex-grow mx-auto mt-8 px-4">
+      <div className="container flex-grow mx-auto mt-8 px-4">
 
-
+        
+        {/* <Metrics/ > */}
         <HistoryList/>
-      </main>
+      </div>
       <Footer/>
     </div>
    );
