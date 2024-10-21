5cc2eb96fb82eff3f4bce61bbeeee3ab4ebe026e
DDublue <theonly1living@gmail.com>
2024-10-21 15:18:20 -0700

    getWorkoutHistory() done + moved some stuff around; still need to fix passing it to a var in history.tsx

--- GymerApp/src/lib/utils.ts
+++ GymerApp/src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatDate = (date: Date): string => {
+  const options: Intl.DateTimeFormatOptions = { 
+    weekday: 'long', 
+    year: 'numeric', 
+    month: 'short', 
+    day: 'numeric'
+  };
+  return date.toLocaleDateString('en-US', options);
+};
