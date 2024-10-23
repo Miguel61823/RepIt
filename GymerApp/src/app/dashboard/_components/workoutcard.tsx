86ab873242febe05c4054e2acb8000394cb8645f
DDublue <theonly1living@gmail.com>
2024-10-22 17:49:28 -0700

    Fixed delete button; need to render on delete instead of refresh page

---
+++ GymerApp/src/app/dashboard/_components/workoutcard.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+// import React, { useEffect, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+ } from "@/components/ui/alert-dialog";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { WorkoutForm } from "../../../components/forms/WorkoutForm";
+import { Workout, getWorkoutHistory, deleteWorkout } from "@/server/api/workouts";
+import { formatDate } from "@/lib/utils";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+// const InsertCard = () => {
+//   return(
+//     <Card className="overflow-hidden flex flex-col h-full">
+//       <CardHeader className="bg-blue-600 text-white">
+//         <CardTitle className="text-xl font-bold truncate">Add New Workout</CardTitle>
+//         <CardDescription className="mt-1 text-white ">Complete this Form</CardDescription>
+//       </CardHeader>
+//       <CardContent className="p-4 flex-grow">
+//         <WorkoutForm />
+//       </CardContent>
+//     </Card>
+//   )
+// }
+
+export const WorkoutCard = ({
+  id,
+  title,
+  description,
+  date_completed,
+  exercises
+}: Workout) => {
+  return (
+    <Card className="overflow-hidden flex flex-col h-full">
+      <CardHeader className="bg-blue-600 text-white">
+        <CardTitle className="text-xl font-bold truncate">{title} {id}</CardTitle>
+        <CardDescription className="mt-1 text-white ">{formatDate(date_completed)}</CardDescription>
+      </CardHeader>
+      <CardContent className="p-4 flex-grow mb-6">
+        <p className="text-sm mb-3">{description}</p>
+        <div className="space-y-2">
+          <ul className="space-y-2">
+            {exercises.map((exercise) => (
+              <li key={exercise.e_id} className="text-sm">
+                <span className="font-semibold">{exercise.name}</span>
+                <ul className="pl-4 mt-1 space-y-1">
+                  {exercise.sets.map((set, index) => (
+                    <li key={index} className="text-xs">
+                      Set {index + 1}: {set.reps} reps @ {set.weight}lbs
+                      {set.notes && <span className="ml-2 text-gray-500">{'~'} {set.notes}</span>}
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <div className="w-full flex justify-between">
+          <Button>
+            Edit
+          </Button>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive">
+                Delete
+              </Button>
+              </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete your workout data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <div className="flex w-full justify-between px-24">
+                <AlertDialogAction
+                  onClick={()=>deleteWorkout(id)}
+                >
+                  Yes, I am sure
+                </AlertDialogAction>
+                <AlertDialogCancel>
+                  Cancel
+                </AlertDialogCancel>
+              </div>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
