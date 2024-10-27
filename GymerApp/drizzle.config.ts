f332b40a48114bbb1811c3a4dcc580c6ca0b714f
DDublue <theonly1living@gmail.com>
2024-10-26 18:05:46 -0700

    Fixed bug: unable to migrate due to incorrect paths in drizzle.config.ts

--- GymerApp/drizzle.config.ts
+++ GymerApp/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './src/drizzle/migrations',
-  schema: ['./src/drizzle/schema/tables/*', './src/drizzle/schema/relations/*', './src/drizzle/schema/types'],
+  schema: ['./src/drizzle/schema/tables/*', './src/drizzle/schema/relations/*', './src/drizzle/schema/types.ts'],
   dialect: 'postgresql',
   strict: true,
   verbose: true,
