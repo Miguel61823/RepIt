d3c998b3a3cb3ce785b3aabd93c31a81bbb0b3ee
DDublue <theonly1living@gmail.com>
2024-10-28 12:29:30 -0700

    Added nav button for gyms (no page yet)

--- GymerApp/src/app/_components/TopBar.tsx
+++ GymerApp/src/app/_components/TopBar.tsx
@@ -3,6 +3,7 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, useAuth, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const Topbar = () => {
@@ -18,11 +19,22 @@ const Topbar = () => {
       router.push('/')
     }
   }
+  const handleGymClick = () => {
+    if (isSignedIn) {
+      //redirect to gyms page
+      router.push('/gyms')
+    } else {
+      router.push('/')
+    }
+  }
 
   return ( 
     <header className="bg-white dark:bg-black dark:text-white text-black p-4 ">
     <div className="container mx-auto flex items-center justify-between">
-      <Button variant="logo" onClick={handleLogoClick}>RepIt</Button>
+      <div className="items-center justify-between">
+        <Button variant="logo" onClick={handleLogoClick}>RepIt</Button>
+        <Button variant="link" onClick={handleGymClick}>Gyms</Button>
+      </div>
       <div className="items-center justify-between">
         <SignedIn>
             <UserButton/>
