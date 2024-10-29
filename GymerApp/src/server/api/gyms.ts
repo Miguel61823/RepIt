a53ff312a6e1cb7e1a52c04c446a3212bc97fe46
DDublue <theonly1living@gmail.com>
2024-10-28 23:20:22 -0700

    Updated getGyms() to filter mockdata (LATER DO DB)

--- GymerApp/src/server/api/gyms.ts
+++ GymerApp/src/server/api/gyms.ts
@@ -1,20 +1,22 @@
 import { db } from "@/drizzle/db";
 import { GymsTable } from "@/drizzle/schema/tables/gym";
+import { mockGyms } from "@/app/gyms/page";
 
-export interface Gym {
+interface Gym {
   gym_id: string;
   name: string;
   address: string;
 }
 
-export async function getGyms(query: string): Promise<Gym[]> {
-  const gyms: Gym[] = [];
-
-  // const test: Gym = {
-  //   gym_id: "1i9ef23test",
-  //   name: "Test Gym",
-  //   address: "123 Test Street, Testville"
-  // };
-  // gyms.push();
+// TODO: FETCH FROM DB INSTEAD OF MOCK DATA
+export async function getGyms(query: string | undefined) {
+  if (!query) {
+    return mockGyms;
+  }
+  const gyms = mockGyms.filter(gym => 
+    gym.name.toLowerCase().includes(query) ||
+    gym.features.some(feature => feature.toLowerCase().includes(query))
+  )
+  gyms.forEach(gym=>{console.log(gym.name)});
   return gyms;
 };
