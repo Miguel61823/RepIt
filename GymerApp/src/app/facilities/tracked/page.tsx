588d2aee609be124f2ff52db6d5fc289c060d01a
David <theonly1living@gmail.com>
2024-11-03 13:43:37 -0800

    Updated layout

--- GymerApp/src/app/facilities/tracked/page.tsx
+++ GymerApp/src/app/facilities/tracked/page.tsx
@@ -1,7 +1,5 @@
 import React, { Suspense } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
-import Topbar from '@/app/_components/TopBar';
-import Footer from '@/app/_components/Footer';
 import { FacilitySearchBar } from '../_components/facilitySearchBar';
 import FacilityListings from '../_components/facilityListings';
 
@@ -21,18 +19,12 @@ export default async function ListingsPage({
     : undefined;
 
   return (
-    <div className="flex flex-col dark:bg-black  bg-white min-h-screen  dark:text-white text-black">
-      <Topbar />
-
-      <div className="container flex-grow max-w-4xl mx-auto mt-8 px-4 mb-8">
-        <Suspense fallback={<div>Loading...</div>}>
-          <FacilitySearchBar search={searchTerm} searchType={"Tracked"} />
-        
-          <FacilityListings search={searchTerm} searchType={"Tracked"} />
-        </Suspense>
-      </div>
-
-      <Footer />
+    <div className="container flex-grow max-w-4xl mx-auto mt-8 px-4 mb-8">
+      <Suspense fallback={<div>Loading...</div>}>
+        <FacilitySearchBar search={searchTerm} searchType={"Tracked"} />
+      
+        <FacilityListings search={searchTerm} searchType={"Tracked"} />
+      </Suspense>
     </div>
   );
 }
