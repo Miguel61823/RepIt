331f120be52b056cdce3c8e720f807818377ae75
David <theonly1living@gmail.com>
2024-11-03 19:27:26 -0800

    Updated db

--- GymerApp/src/drizzle/schema/tables/facilities.ts
+++ GymerApp/src/drizzle/schema/tables/facilities.ts
@@ -1,4 +1,4 @@
-import { pgTable, uuid, text, integer, jsonb, decimal, index } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, integer, jsonb, decimal, index, doublePrecision } from "drizzle-orm/pg-core";
 
 // TODO: ADD FIELDS LISTED IN CHAT
 export const FacilitiesTable = pgTable("facility", {
@@ -6,8 +6,8 @@ export const FacilitiesTable = pgTable("facility", {
   osm_id: text("osm_id").notNull(),
   name: text("name").notNull(),
   leisure: text("leisure").notNull(),
-  lat: decimal("lat", { precision: 10, scale: 7}).notNull(),
-  lon: decimal("lon", { precision: 10, scale: 7}).notNull(),
+  lat: doublePrecision("lat").notNull(),
+  lon: doublePrecision("lon").notNull(),
   address: text("address").notNull(),
   accessibility: text("accessibility").notNull(),
   opening_hours: text("opening_hours"),
