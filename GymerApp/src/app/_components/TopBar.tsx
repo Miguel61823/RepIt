3ae6d1568e91b9e313ce72a5961f9fa36c7d97a8
DDublue <theonly1living@gmail.com>
2024-11-04 01:02:15 -0800

    Navbar mobile-friendly

--- GymerApp/src/app/_components/TopBar.tsx
+++ GymerApp/src/app/_components/TopBar.tsx
@@ -10,7 +10,9 @@ import {
   UserButton,
 } from '@clerk/nextjs';
 import {useRouter} from 'next/navigation';
-import {dark} from '@clerk/themes';
+import {dark, experimental__simple} from '@clerk/themes';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Menu } from 'lucide-react';
 
 const Topbar = () => {
   const router = useRouter();
@@ -51,13 +53,13 @@ const Topbar = () => {
   };
 
   return (
-    <header className="hidden md:block z-50 sticky top-0 bg-gray-200 dark:bg-gray-800 dark:text-white text-black py-4 border-b">
-      <div className="relative flex items-center text-violet-600 px-4">
-        <Button variant="logo" onClick={handleLogoClick}>
-          RepIt
-        </Button>
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-center font-sans">
+    <>
+      <header className="block z-50 sticky top-0 bg-gray-200 dark:bg-gray-800 dark:text-white text-black py-4 border-b">
+        <div className="relative flex justify-between items-center text-violet-600 px-2">
+          <Button variant="logo" onClick={handleLogoClick}>
+            RepIt
+          </Button>
+          <div className="hidden min-[600px]:flex items-center font-sans">
             <Button variant="link" onClick={handleFacilityClick}>
               Facilities
             </Button>
@@ -68,31 +70,77 @@ const Topbar = () => {
               Sessions
             </Button>
           </div>
-          <div className="flex items-center">
-            <div className="px-3">
-              <SignedIn>
-                <UserButton
-                  appearance={{
-                    baseTheme: [dark],
-                  }}
-                />
-              </SignedIn>
+          <div className="flex items-center justify-end w-full">
+            <div className="min-[600px]:hidden flex gap-3 px-4 items-center">
+              <div className="px-3">
+                <SignedIn>
+                  <UserButton/>
+                </SignedIn>
 
-              <SignedOut>
-                <SignInButton mode="modal">
-                  <button className="bg-violet-600 px-4 py-2 rounded-md text-white">
-                    Sign In
-                  </button>
-                </SignInButton>
-              </SignedOut>
+                <SignedOut>
+                  <SignInButton mode="modal">
+                    <button className="bg-violet-600 px-4 py-2 rounded-lg text-white">
+                      Sign In
+                    </button>
+                  </SignInButton>
+                </SignedOut>
+              </div>
+              <DropdownMenu modal={false}>
+                <DropdownMenuTrigger asChild className="cursor-pointer">
+                  <Menu />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="font-sans bg-gray-200 dark:bg-gray-950 dark:text-white text-black">
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem className="">
+                      <Button variant="link" onClick={handleFacilityClick}>
+                        Facilities
+                      </Button>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Button variant="link" onClick={handleWorkoutClick}>
+                        Workouts
+                      </Button>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Button variant="link" onClick={handleSessionClick}>
+                        Sessions
+                      </Button>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup className="flex flex-row justify-end px-1">
+                    <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                      <ModeToggle />
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
-            <div>
-              <ModeToggle />
+            <div className="hidden min-[600px]:flex items-center">
+              <div className="px-3">
+                <SignedIn>
+                  <UserButton/>
+                </SignedIn>
+
+                <SignedOut>
+                  <SignInButton mode="modal">
+                    <button className="bg-violet-600 px-4 py-2 rounded-md text-white">
+                      Sign In
+                    </button>
+                  </SignInButton>
+                </SignedOut>
+              </div>
+              <div>
+                <ModeToggle />
+              </div>
             </div>
           </div>
+          <div>
+
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
