b504c5b8823d15e7e195167f1a5b5eeb640215d1
Miguel Mascareno <mmascare@ucsc.edu>
2024-11-18 12:26:05 -0800

    delted old files

--- AccountService/src/server.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-import app from './app';
-
-const port = process.env.ACCOUNT_SERVICE_PORT || 5401;
-app.listen(port, () => {
-  console.log(`Server Running on port ${port}`);
-})
