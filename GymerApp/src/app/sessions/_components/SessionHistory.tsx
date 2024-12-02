1b27b3465c3d53b779461cb590e3c4546708169d
Miguel Mascareno <mmascare@ucsc.edu>
2024-12-02 14:31:27 -0800

    new eslintrc

--- GymerApp/src/app/sessions/_components/SessionHistory.tsx
+++ GymerApp/src/app/sessions/_components/SessionHistory.tsx
@@ -27,11 +27,6 @@ const SessionHistory = async () => {
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
             <SessionsList />
-            {/* <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-              {(await workouts).map(session => (
-                <SessionCard key={session.session_id} {...session} />
-              ))}
-            </div> */}
           </div>
         </div>
       </main>
